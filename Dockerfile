10815fe44b21e1d64c6360312562c77e994ff030
Alexander Efimov <alexander.efimov@dataart.com>
2016-08-11 14:50:42 +0300

    Added mount modes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM alpine:latest
 RUN apk update
-RUN apk add nfs-utils
+RUN apk add nfs-utils bash
 RUN rm -rf /var/cache/apk/*
 
 ENTRYPOINT ["/bin/bash", "/entrypoint.sh" ]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,50 @@
-#!/bin/sh
+#!/bin/bash
 set -e
 
-FS_ID="$1"
-MNT_DIR="${2:-/mnt/efs/${FS_ID}}"
-
-[ -d "${MNT_DIR}" ] || mkdir -p "${MNT_DIR}"
+MOUNT_MODE='standard'
+MOUNT_DIR='/mnt'
 
 EC2_AZ=`wget -q -O- http://169.254.169.254/latest/meta-data/placement/availability-zone`
-EC2_REGION="${EC2_AZ%?}"
 
-rpcbind
+while getopts ':m:d:' opt; do
+  case "${opt}" in
+    m)
+      MOUNT_MODE="${OPTARG}"
+    ;;
+    d)
+      MOUNT_DIR="${OPTARG}"
+    ;;
+    *)
+      echo "Options error"
+  esac
+done
+shift $(($OPTIND - 1))
 
-mount -t nfs4 -o nfsvers=4.1,nolock "${EC2_AZ}.${FS_ID}.efs.${EC2_AZ%?}.amazonaws.com:/" "${MNT_DIR}"
+while [ $# -gt 0 ]; do
+  FS_ID="$1"
+  MNT="${MOUNT_DIR}/${FS_ID}"
+
+  ENDPOINT="${EC2_AZ}.${FS_ID}.efs.${EC2_AZ%?}.amazonaws.com:/"
+  case "${MOUNT_MODE}" in
+    rancheros)
+      echo "mounts: [ ['${ENDPOINT}', '${MNT}', 'nfs4', 'nfsvers=4.1,nolock'] ]" | ros config merge
+    ;;
+    standard)
+      echo -n "Mounting ${ENDPOINT} to ${MNT}... "
+      [ -d "${MNT}" ] || mkdir -p "${MNT}"
+      rpcbind
+      mount -t nfs4 -o nfsvers=4.1,nolock "${ENDPOINT}" "${MNT}"
+      if [ $? -ne 0 ]; then
+        echo "Error"
+      else
+        echo "Success"
+      fi
+    ;;
+    *)
+      echo "Unsupported mount mode ${MOUNT_MODE}"
+      echo "Supported mount modes: rancheros, standard"
+      exit 1
+    ;;
+  esac
+done
+echo "End of options"
